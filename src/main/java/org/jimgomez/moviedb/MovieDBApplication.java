afc8744cf575074f5b3fee3ddcdf1019495f4e05
Jim Gomez <jim.gomez.48@gmail.com>
2025-06-23 22:10:29 -0700

    Initial commit

---
+++ src/main/java/org/jimgomez/moviedb/MovieDBApplication.java
@@ -0,0 +1,13 @@
+package org.jimgomez.moviedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieDBApplication.class, args);
+    }
+
+}
